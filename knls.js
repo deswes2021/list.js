93bd52c78eeabbc4fa564ddcdda266b26a189bf2
deswes2021 <89219703+deswes2021@users.noreply.github.com>
2024-11-20 20:58:27 -0600

    Update knls.js

--- knls.js
+++ knls.js
@@ -1578,6 +1578,11 @@
         tipo:'pelis'
     },
     {
+        url:'https://archive.org/serve/shrek_202407/Shrek.avi',
+        logo:'https://raw.githubusercontent.com/deswes2021/list.js/main/pelis/shrek1.jpg',
+        tipo:'pelis'
+    },    
+    {
         url:'https://archive.org/serve/shrek_202407/Shrek%202.mp4',
         logo:'https://raw.githubusercontent.com/deswes2021/list.js/main/pelis/shrek2.jpg',
         tipo:'pelis'
